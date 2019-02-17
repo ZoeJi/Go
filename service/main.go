70722ea83d2b199129a6fbe6ed0d8c302507648c
ZoeJi <zoe.q.ji@gmail.com>
2019-02-16 22:28:09 -0800

    added main

--- service/main.go
+++ service/main.go
@@ -1,28 +1,39 @@
 package main
 
-import "fmt"
+import(
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
 type Location struct {
-      Lat float64 `json:"lat"`
-      Lon float64 `json:"lon"`
+	Lat float64 `json:"lat"`
+	Lon float64 `json:"lon"`
 }
 
 type Post struct {
       // `json:"user"` is for the json parsing of this User field. Otherwise, by default it's 'User'.
-      User     string `json:"user"`
-      Message  string  `json:"message"`
-      Location Location `json:"location"`
+	User     string `json:"user"`
+	Message  string  `json:"message"`
+	Location Location `json:"location"`
 }
 
 func handlerPost(w http.ResponseWriter, r *http.Request) {
       // Parse from body of request to get a json object.
-      fmt.Println("Received one post request")
-      decoder := json.NewDecoder(r.Body)
-      var p Post
-      if err := decoder.Decode(&p); err != nil {
-             panic(err)
-             return
-      }
+	fmt.Println("Received one post request")
+	decoder := json.NewDecoder(r.Body)
+	var p Post
+	if err := decoder.Decode(&p); err != nil {
+        	panic(err)
+        	return
+	}
 
-      fmt.Fprintf(w, "Post received: %s\n", p.Message)
+	fmt.Fprintf(w, "Post received: %s\n", p.Message)
 }
 
+func main() {
+	fmt.Println("started-service")
+	http.HandleFunc("/post", handlerPost)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
